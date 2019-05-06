b8bcc7b729e7e856b351d75c61b590903efdfaa7
gustafeden <gustafeden@gmail.com>
2019-05-06 11:35:04 +0200

    branch 1 commit

---
+++ gittestproject/src/main/java/server/Main.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package server;
+
+/**
+ *
+ * @author gusta
+ */
+public class Main {
+    int noooo;
+    public Main(){
+        noooo = 1;
+    }
+}
